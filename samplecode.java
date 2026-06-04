771952d444a66097fa68699c823d7400430dbf89
GopiStarkk <gopi.vjf@gmail.com>
2026-06-04 11:19:22 +0530

    code changed in feature branch - font color changed

--- samplecode.java
+++ samplecode.java
@@ -5,22 +5,4 @@ import java.io.collection;
 import java.io.scanner;
 import java.io.outputprinter;
 import java.io.inputprinter;
-public class SpiralPatternExample2
-{  
-//function to print the spiral pattern  
-public static void printPattern(int n)  
-{  
-//detrmines the boundary size of the array  
-int size = 2 * n - 1;  
-//inner loop  
-for(int i = 1; i <= size; i++)  
-{  
-//outer loop      
-for(int j = 1; j <= size; j++)  
-{  
-//calculates and prints the values for pattern  
-System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
-}  
-System.out.println();  
-}  
-}  
+****CODE CHANGED******  
